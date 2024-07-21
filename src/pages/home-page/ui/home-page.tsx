fd3fc2ff8d928de4cbf067658975c698b8a3553c
Anton Skalaban <antonskalaban99@gmail.com>
2024-07-21 22:24:41 +0300

    feat: add facilite section

--- src/pages/home-page/ui/home-page.tsx
+++ src/pages/home-page/ui/home-page.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 
 import HomeHeader from "./home-header/ui/home-header";
+import OurFacilite from "./our-facilities/ui/our-facilities";
 
 const HomePage: FC = () => {
   return (
     <>
       <HomeHeader />
+      <OurFacilite />
     </>
   );
 };
---
+++ src/pages/home-page/ui/our-facilities/ui/our-facilities.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { useLocale, useTranslations } from "next-intl";
+
+import { Locale } from "shared/lib/i18n";
+import { Wrapper } from "shared/ui/wrapper";
+
+import { cards } from "../model/constants";
+import styles from "./styles.module.scss";
+
+const OurFacilite: FC = () => {
+  const localActive = useLocale() as Locale;
+  const t = useTranslations("home.facilite");
+
+  return (
+    <Wrapper>
+      <section className={styles.facilite}>
+        <h2 className={styles.facilite__title}>{t("title")}</h2>
+        <p className={styles.facilite__subtitle}>{t("subtitle")}</p>
+
+        <div className={styles["cards-container"]}>
+          {cards.map(({ title, image }) => (
+            <div className={styles.card} key={title[localActive]}>
+              {image}
+              <p className={styles["card__title"]}>{title[localActive]}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </Wrapper>
+  );
+};
+
+export default OurFacilite;
