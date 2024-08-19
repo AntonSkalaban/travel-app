e99e22e323fa724fde50c131f70ae2d9f7f52185
Anton Skalaban <antonskalaban99@gmail.com>
2024-08-19 23:05:15 +0300

    feat: add prettier check

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,42 +1,47 @@
-import { StorybookConfig } from '@storybook/nextjs';
-const path = require('path');
- 
+import { StorybookConfig } from "@storybook/nextjs";
+const path = require("path");
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ['storybook-next-intl'],
+  addons: ["storybook-next-intl"],
   webpackFinal: async (config) => {
-
     config.module = config.module || {};
     config.module.rules = config.module.rules || [];
-  
+
     // This modifies the existing image rule to exclude .svg files
     // since you want to handle those files with @svgr/webpack
-    const imageRule = config.module.rules.find((rule) => rule?.['test']?.test('.svg'));
+    const imageRule = config.module.rules.find((rule) =>
+      rule?.["test"]?.test(".svg"),
+    );
     if (imageRule) {
-      imageRule['exclude'] = /\.svg$/;
+      imageRule["exclude"] = /\.svg$/;
     }
-  
+
     // Configure .svg files to be loaded with @svgr/webpack
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
-  
 
-    if(config?.resolve?.alias) {
-      config.resolve.alias['features'] = path.resolve(__dirname, '../src/features');
-      config.resolve.alias['widgets'] = path.resolve(__dirname, '../src/widgets');
-      config.resolve.alias['shared'] = path.resolve(__dirname, '../src/shared');
-      config.resolve.alias['app'] = path.resolve(__dirname, '../src/app');
-  }
+    if (config?.resolve?.alias) {
+      config.resolve.alias["features"] = path.resolve(
+        __dirname,
+        "../src/features",
+      );
+      config.resolve.alias["widgets"] = path.resolve(
+        __dirname,
+        "../src/widgets",
+      );
+      config.resolve.alias["shared"] = path.resolve(__dirname, "../src/shared");
+      config.resolve.alias["app"] = path.resolve(__dirname, "../src/app");
+    }
     return config;
   },
 
   staticDirs: [
     {
-      from: '../src/shared/ui/fonts',
-      to: 'src/shared/ui/fonts',
+      from: "../src/shared/ui/fonts",
+      to: "src/shared/ui/fonts",
     },
   ],
 
@@ -44,18 +49,18 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
     options: {
       builder: {
-        useSWC: true
-      }
-    }
+        useSWC: true,
+      },
+    },
   },
   swc: () => ({
     jsc: {
       transform: {
         react: {
-          runtime: 'automatic'
-        }
-      }
-    }
+          runtime: "automatic",
+        },
+      },
+    },
   }),
   docs: {
     autodocs: "tag",
